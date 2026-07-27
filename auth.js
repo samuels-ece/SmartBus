2bcb2f604415cf86058dcf02e95b5ede049b526f
samuels-ece <samuelabi2006@gmail.com>
2026-07-27 16:56:51 +0530

    Implement user login with role-based redirects
    Added login functionality with role-based redirection.

--- auth.js
+++ auth.js
@@ -1,1 +1,37 @@
+import { auth } from "./firebase.js";
 
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", () => {
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const role = document.getElementById("role").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+
+    .then(() => {
+
+        if(role === "student"){
+            window.location.href = "student.html";
+        }
+
+        else if(role === "driver"){
+            window.location.href = "driver.html";
+        }
+
+        else{
+            window.location.href = "admin.html";
+        }
+
+    })
+
+    .catch((error)=>{
+        alert(error.message);
+    });
+
+});
