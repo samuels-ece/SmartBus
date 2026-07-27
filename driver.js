2dbd0ff3ef3227b3ceeb4b272b5d097655f6c5f9
samuels-ece <samuelabi2006@gmail.com>
2026-07-27 17:20:48 +0530

    Add geolocation feature to start trip

--- driver.js
+++ driver.js
@@ -1,1 +1,35 @@
+const startTrip = document.getElementById("startTrip");
 
+startTrip.addEventListener("click", () => {
+
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(
+
+            (position) => {
+
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+
+                document.getElementById("tripStatus").innerText = "Running";
+
+                document.getElementById("driverLocation").innerText =
+                    latitude + ", " + longitude;
+
+            },
+
+            () => {
+
+                alert("Unable to get location.");
+
+            }
+
+        );
+
+    } else {
+
+        alert("Geolocation is not supported.");
+
+    }
+
+});
