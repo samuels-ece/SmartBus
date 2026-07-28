d1f3d2e55b672e8ac77e7cac3b5ecf8349f7149f
samuels-ece <samuelabi2006@gmail.com>
2026-07-28 17:23:56 +0530

    Update bus icon and shadow properties

--- student.js
+++ student.js
@@ -24,12 +24,14 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 // ==========================
 
 const busIcon = L.icon({
-    iconUrl: "https://cdn-icons-png.flaticon.com/512/61/61231.png",
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-    popupAnchor: [0, -35]
-});
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
 
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
 const busMarker = L.marker(
     [13.0827, 80.2707],
     { icon: busIcon }
