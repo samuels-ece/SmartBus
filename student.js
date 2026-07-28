fcbe773b9ee55d643f9de0c0205d25c8e50899fa
samuels-ece <samuelabi2006@gmail.com>
2026-07-28 08:04:50 +0530

    Add alerts for debugging in student.js

--- student.js
+++ student.js
@@ -14,42 +14,51 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 // ==========================
-// Create Map
+// Before Map
 // ==========================
+
+alert("Before Map");
 
 const map = L.map("map").setView([13.0827, 80.2707], 13);
 
+alert("After Map");
+
+// OpenStreetMap
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "&copy; OpenStreetMap contributors"
 }).addTo(map);
+
+alert("Tile Layer Loaded");
 
 // ==========================
 // Bus Marker
 // ==========================
 
+alert("Before Marker");
+
 const busMarker = L.marker([13.0827, 80.2707]).addTo(map);
+
+alert("After Marker");
 
 busMarker.bindPopup("🚌 SmartBus");
 
 // ==========================
-// Load Bus Stops
+// Load Stops
 // ==========================
 
 async function loadStops() {
 
-    alert("Step 1");
+    alert("Inside loadStops()");
 
     try {
 
-        alert("Step 2");
+        alert("Creating Collection");
 
         const stopsRef = collection(db, "routes", "bus1", "stops");
 
-        alert("Step 3");
+        alert("Reading Firestore");
 
         const snapshot = await getDocs(stopsRef);
-
-        alert("Step 4");
 
         alert("Documents Found: " + snapshot.size);
 
@@ -67,15 +76,21 @@ async function loadStops() {
 
     } catch (error) {
 
-        alert("ERROR");
+        alert("Firestore Error");
+
         alert(error.message);
-        console.error(error);
+
+        console.log(error);
 
     }
 
 }
 
+alert("Before loadStops()");
+
 loadStops();
+
+alert("After loadStops()");
 
 // ==========================
 // Live Bus Updates
@@ -84,8 +99,11 @@ loadStops();
 onSnapshot(doc(db, "bus", "live"), (docSnap) => {
 
     if (!docSnap.exists()) {
+
         document.getElementById("status").innerText = "Bus Offline";
+
         return;
+
     }
 
     const data = docSnap.data();
@@ -98,6 +116,7 @@ onSnapshot(doc(db, "bus", "live"), (docSnap) => {
 
         document.getElementById("location").innerText =
             data.latitude.toFixed(6) + ", " + data.longitude.toFixed(6);
+
     }
 
     document.getElementById("status").innerText =
@@ -113,10 +132,14 @@ document.getElementById("logoutBtn").addEventListener("click", () => {
 
     signOut(auth)
         .then(() => {
+
             window.location.href = "login.html";
+
         })
         .catch((error) => {
+
             alert(error.message);
+
         });
 
 });
